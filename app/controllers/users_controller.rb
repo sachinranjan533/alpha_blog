3333018113f75b808fbf3a1471ba5e14040d4036
sachin_ranjan <sachin.ranjan@unthinkable.co>
2021-11-26 16:53:43 +0530

    add user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
     def new
         @user=User.new
     end
+    def create
+        @user=User.new(params.require(:user).permit(:username,:email,:password))
+        if @user.save
+            flash[:notice]="Welcome to the Alpha Blog #{@user.username}"
+            redirect_to articles_path
+        else
+            render 'new'
+        end
+    end
 end
