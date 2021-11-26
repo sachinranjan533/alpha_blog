2b7a0fe8c4b097342bb53ae3437d515f8c0b7bf9
sachin_ranjan <sachin.ranjan@unthinkable.co>
2021-11-26 17:46:35 +0530

    edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,16 @@ class UsersController < ApplicationController
             render 'new'
         end
     end
+    def edit
+        @user=User.find(params[:id])
+    end
+    def update
+        @user=User.find(params[:id])
+        if @user.update(params.require(:user).permit(:username,:email,:password))
+            flash[:notice]="User data updated successfully"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end
+    end
 end
