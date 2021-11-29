b7f4afa97be335d7fb4171c134e803a1c79d34ef
sachin_ranjan <sachin.ranjan@unthinkable.co>
2021-11-29 12:58:38 +0530

    show user and profile image and add user index

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+    def gravatar_for(user,size: 80)
+        hash = Digest::MD5.hexdigest("sachin.ranjan@unthinkable.co")
+        image_src = "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
+        image_tag(image_src, alt: user.username,class: "rounded shadow mx-auto d-block")
+    end
 end
