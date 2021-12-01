542a807a1f227a3b6784c7d21196523c9b84cb7e
sachin_ranjan <sachin.ranjan@unthinkable.co>
2021-12-01 11:31:26 +0530

    added validations and test for validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,4 @@
 class Category < ApplicationRecord
+    # adding validation to our category model
+    validates :name,presence: true,uniqueness: {case_sensitive: false},length: {minimum: 10,maximum: 20}
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,8 +1,39 @@
 require  'test_helper'
 
 class CategoryTest <ActiveSupport::TestCase
-    test "category should be valid" do
-        @category=Category.new(name: "news")
-        assert @category.valid?
+
+    #this methods runs before every test
+    def setup
+        @category=Category.new(name: "sports")
     end
+
+    test "category should be valid" do
+        assert @category.valid? # ensure that test is true
+    end
+
+    # defining test which check whether name is present or not
+    test "name should be present" do
+        @category.name=" "
+        assert_not @category.valid? #ensure that test is false
+    end
+
+    # defining test which check whether name is unique or not
+    test "name should be unique" do
+        @category.save #first we need to stor in test DB to check uniqueness.
+        @category2=Category.new(name: "news")
+        assert_not @category2.valid?
+    end
+
+    # defining test which check whether name is not too long
+    test "name should not be too long" do
+        @category.name="a"*26
+        assert_not @category.valid?
+    end
+
+    # defining test which check whether name is not too short
+    test "name should not be too short" do
+        @category.name="aa"
+        assert_not @category.valid?
+    end
+
 end
