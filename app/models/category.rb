67d27c1091c5f31fef57d97d0997d2f473e7349f
sachin_ranjan <sachin.ranjan@unthinkable.co>
2021-12-02 11:42:30 +0530

    implemented many to many association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
     # adding validation to our category model
     validates :name,presence: true,uniqueness: {case_sensitive: false},length: {minimum: 3,maximum: 20}
+    has_many :article_categories
+    has_many :articles,through: :article_categories
 end
